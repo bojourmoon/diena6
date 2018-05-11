3bb2cbb2a4c13dcb72cc23653a9eb171dc382e56
Emilija <misssozolina@gmail.com>
2018-05-11 20:03:25 +0300

    4uzdevums
    4 uzdevums

---
+++ diena6/diena6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diena6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Ievadi savu laimigo skaitli***");
+            string ievads = Console.ReadLine();
+            Console.WriteLine(Ievadaskaitli(ievads));
+            Console.ReadLine();
+        }
+        static string Ievadaskaitli(string skaitlis)
+
+        {
+
+
+            string rezultats = "Tavs laimigais skaitlis ir ----->  " + skaitlis;
+
+            return rezultats;
+        }
+    }
+    
+    
+
+}
+
+
+
+
+
